0ec8aa11dab3a6f9758824c6d09712941edc1a4e
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2025-04-15 17:39:39 +0300

    commit anagram assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
-# your code goes here!
+# anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, word_list):
+        matches = []
+        for candidate in word_list:
+            if candidate.lower() != self.word and sorted(candidate.lower()) == self.sorted_word:
+                matches.append(candidate)
+        return matches
+
+
+listen = Anagram("listen")
+result = listen.match(['enlists', 'google', 'inlets', 'banana'])
+print(result)
+
+
